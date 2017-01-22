857bed1f1b679a269bbe68759669b4bb74b65486
Jared Wolens <jwolens@stanford.edu>
2017-01-21 16:12:02 -0800

    feat: add Mongo functionality and User schema

--- imports/components/views/homeScreen.js
+++ imports/components/views/homeScreen.js
@@ -1,26 +1,18 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
+import { Users } from '../../api/users.js';
+
 import template from './homeScreen.html';
  
 class MusitHomeCtrl {
-  constructor() {
-    this.recs = [{
-      text: 'This is rec 1',
-      img: 'http://f4.bcbits.com/img/a3753111161_10.jpg',
-      recm: {
-        track: 'Hey Lover',
-        artist: 'Village, INCA',
-        album: ''
-      },
-      friend: {
-        name: 'James Geary',
-        img: 'https://scontent-sjc2-1.xx.fbcdn.net/v/t1.0-9/15697762_1755526128044941_128235100197194746_n.jpg?oh=40b862ef949c9aacdccd3169200642c9&oe=591B55C5'
+  constructor($scope) {
+    $scope.viewModel(this);
+ 
+    this.helpers({
+      users() {
+        return Users.find({});
       }
-    }, {
-      text: 'This is rec 2'
-    }, {
-      text: 'This is rec 3'
-    }];
+    });
   }
 }
  
@@ -29,5 +21,24 @@ export default angular.module('musitHome', [
 ])
   .component('musitHome', {
     templateUrl: 'imports/components/views/homeScreen.html',
-    controller: MusitHomeCtrl
+    controller: ['$scope', MusitHomeCtrl]
   });
+
+
+// this.recs = [{
+//   text: 'This is rec 1',
+//   img: 'http://f4.bcbits.com/img/a3753111161_10.jpg',
+//   recm: {
+//     track: 'Hey Lover',
+//     artist: 'Village, INCA',
+//     album: ''
+//   },
+//   friend: {
+//     name: 'James Geary',
+//     img: 'https://scontent-sjc2-1.xx.fbcdn.net/v/t1.0-9/15697762_1755526128044941_128235100197194746_n.jpg?oh=40b862ef949c9aacdccd3169200642c9&oe=591B55C5'
+//   }
+// }, {
+//   text: 'This is rec 2'
+// }, {
+//   text: 'This is rec 3'
+// }];
