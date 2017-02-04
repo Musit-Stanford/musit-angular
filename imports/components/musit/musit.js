7db74ea937ed8a1e4e9ee5fc2be1cf3ab01b6b80
James Geary <james@stanford.edu>
2017-02-04 12:47:26 -0800

    even better

--- imports/components/musit/musit.js
+++ imports/components/musit/musit.js
@@ -19,98 +19,100 @@ class Musit {}
 const name = 'musit';
 
 // create a module
-export default angular.module(name, [
-  angularMeteor,
-  uiRouter,
-  Trending.name,
-  MusitHome.name,
-  Recommendation.name,
-  createRecommendation.name,
-  friendList.name,
-  trendingList.name,
-  success.name,
-  playlist.name,
-  createPlaylist.name,
-  playlistList.name
-]).component(name, {
-  templateUrl: template,
-  erAs: name,
-  controller: Musit
-})
-  .config(configfunc);
+(function () {
+  export default angular.module(name, [
+    angularMeteor,
+    uiRouter,
+    Trending.name,
+    MusitHome.name,
+    Recommendation.name,
+    createRecommendation.name,
+    friendList.name,
+    trendingList.name,
+    success.name,
+    playlist.name,
+    createPlaylist.name,
+    playlistList.name
+  ]).component(name, {
+    templateUrl: template,
+    erAs: name,
+    controller: Musit
+  })
+    .config(config);
 
-var configfunc = function ($locationProvider, $urlRouterProvider, $stateProvider) {
-  $locationProvider.html5Mode(true);
+  function config($locationProvider, $urlRouterProvider, $stateProvider) {
+    $locationProvider.html5Mode(true);
 
-  $urlRouterProvider.otherwise('/');
+    $urlRouterProvider.otherwise('/');
 
-  var homeState = {
-    name: 'home',
-    url: '/',
-    template: '<home></home>'
+    var homeState = {
+      name: 'home',
+      url: '/',
+      template: '<home></home>'
+    }
+    $stateProvider.state(homeState);
+
+    var recommendationState = {
+      name: 'recommendation',
+      url: '/recommendation/:recId',
+      template: '<recommendation></recommendation>'
+    }
+    $stateProvider.state(recommendationState);
+
+    var playlistsState = {
+      name: 'playlists',
+      url: '/playlists/:recId',
+      template: '<playlists></playlists>'
+    }
+    $stateProvider.state(playlistsState);
+
+    var createRecommendationState = {
+      name: 'createRecommendation',
+      url: '/createRecommendation',
+      template: '<create-recommendation></create-recommendation>'
+    }
+    $stateProvider.state(createRecommendationState);
+
+    var friendListState = {
+      name: 'friendList',
+      url: '/friendList',
+      template: '<friend-list></friend-list>'
+    }
+    $stateProvider.state(friendListState);
+
+    var successState = {
+      name: 'success',
+      url: '/success',
+      template: '<success></success>'
+    }
+    $stateProvider.state(successState);
+
+    var trendingState = {
+      name: 'trending',
+      url: '/trending',
+      template: '<trending></trending>'
+    }
+    $stateProvider.state(trendingState);
+
+    var trendingListState = {
+      name: 'trendingList',
+      url: '/trendingList',
+      template: '<trending-list></trending-list>'
+    }
+    $stateProvider.state(trendingListState);
+
+    var playlistListState = {
+      name: 'playlistList',
+      url: '/playlistList/:playlistId',
+      template: '<playlist-list></playlist-list>'
+    }
+    $stateProvider.state(playlistListState);
+
+    var createPlaylistState = {
+      name: 'createPlaylist',
+      url: '/createPlaylist',
+      template: '<create-playlist></create-playlist>'
+    }
+    $stateProvider.state(createPlaylistState);
   }
-  $stateProvider.state(homeState);
-
-  var recommendationState = {
-    name: 'recommendation',
-    url: '/recommendation/:recId',
-    template: '<recommendation></recommendation>'
-  }
-  $stateProvider.state(recommendationState);
-
-  var playlistsState = {
-    name: 'playlists',
-    url: '/playlists/:recId',
-    template: '<playlists></playlists>'
-  }
-  $stateProvider.state(playlistsState);
-
-  var createRecommendationState = {
-    name: 'createRecommendation',
-    url: '/createRecommendation',
-    template: '<create-recommendation></create-recommendation>'
-  }
-  $stateProvider.state(createRecommendationState);
-
-  var friendListState = {
-    name: 'friendList',
-    url: '/friendList',
-    template: '<friend-list></friend-list>'
-  }
-  $stateProvider.state(friendListState);
-
-  var successState = {
-    name: 'success',
-    url: '/success',
-    template: '<success></success>'
-  }
-  $stateProvider.state(successState);
-
-  var trendingState = {
-    name: 'trending',
-    url: '/trending',
-    template: '<trending></trending>'
-  }
-  $stateProvider.state(trendingState);
-
-  var trendingListState = {
-    name: 'trendingList',
-    url: '/trendingList',
-    template: '<trending-list></trending-list>'
-  }
-  $stateProvider.state(trendingListState);
-
-  var playlistListState = {
-    name: 'playlistList',
-    url: '/playlistList/:playlistId',
-    template: '<playlist-list></playlist-list>'
-  }
-  $stateProvider.state(playlistListState);
-
-  var createPlaylistState = {
-    name: 'createPlaylist',
-    url: '/createPlaylist',
-    template: '<create-playlist></create-playlist>'
-  }
-  $stateProvider.state(createPlaylistState);
-};
+}());
